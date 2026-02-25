7640b9b4dd399f22ed4e3ba908563de63072339e
ambah <Archange.Mbah@milchundzucker.de>
2026-02-25 16:35:48 +0100

    user ist able to upload text

--- app/api/router.py
+++ app/api/router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from app.api.endpoints.web import router as web_router
+from .endpoints.documents import router as documents_router
 
 router = APIRouter()
 router.include_router(web_router)
+router.include_router(documents_router)
